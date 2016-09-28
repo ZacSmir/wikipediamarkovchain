d666e92619baae8a8a4ab4038acad7cc267003ac
ZacSmir <Zach@smirin.us>
2016-09-28 09:17:39 -0700

    Classadoo autocommit

--- program.js
+++ program.js
@@ -1,7 +1,7 @@
 
-//var title = "#game-input".val()
-//console.log(title)
-//getWikiIntro(title, function(text) {
-//  console.log(text)
-//  return text
-//})
+var title = "#game-input".val()
+console.log(title)
+getWikiIntro(title, function(text) {
+  console.log(text)
+  return text
+})
