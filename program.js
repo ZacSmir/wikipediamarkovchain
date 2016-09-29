59a67b9a2eac17fd2493bd34cab4a54ed1a837eb
ZacSmir <Zach@smirin.us>
2016-09-29 14:43:07 -0700

    Classadoo autocommit

--- program.js
+++ program.js
@@ -1,16 +1,19 @@
-gameinput.keydown(function(keydownEvent) {
+var gameInput = $("#game-input");
+var gameOutput  = $("#game-output");
+gameInput.keydown(function(keydownEvent) {
   // the key code for enter is 13
 
   if (keydownEvent.keyCode == 13) {      
-    parseText(gameinput.val()); 
-    gameinput.val("");
-    $("#gameinput").val
+    parseText(gameInput.val()); 
+    gameInput.val("");
   }  
 })
 
-var title = $("#gameinput").val()
-console.log(title)
-getWikiIntro(title, function(text) {
-  console.log(text)
-  return text
-})
+
+
+//var title = $("#gameinput").val()
+//console.log(title)
+//getWikiIntro(title, function(text) {
+//  console.log(text)
+//  return text
+//})
