0f84345b8ef4bd1efa9822ef812ba4979c88175c
ZacSmir <Zach@smirin.us>
2016-10-05 09:20:10 -0700

    Classadoo autocommit

--- program.js
+++ program.js
@@ -17,7 +17,8 @@ gameInput.keydown(function(keydownEvent) {
     })
     
     function get() { return $("#output").value; }
-
+    
+    get()
     // Clears the source textbox
     function clear() { document.getElementById('in').value = ''; }
 
