2d4db79594de9977924fda56962cb4dd6a9f456c
ZacSmir <Zach@smirin.us>
2016-10-10 08:44:23 -0700

    Classadoo autocommit

---
+++ markovchain.js
@@ -0,0 +1,64 @@
+// Returns the source text
+function get() { return $("#output").html(); }
+
+// Clears the source textbox
+function clear() { $("#output").html(""); }
+
+// Writes to the output textbox
+function set(v) { $("#output").html(v) }
+
+// Holds the state information
+var cache = {
+  '_START': []
+};
+
+function _generate() {
+  console.log("Asdf")
+  // Get the source text and split it into words
+  var text = get().split(/\s+/g);
+
+  if (!text.length)
+    return;
+
+  // Add it to the start node's list of possibility
+  cache['_START'].push(text[0]);
+
+  // Now go through each word and add it to the previous word's node
+  for (var i = 0; i < text.length - 1; i++) {
+    if (!cache[text[i]])
+      cache[text[i]] = [];
+    cache[text[i]].push(text[i + 1]);
+
+    // If it's the beginning of a sentence, add the next word to the start node too
+    if (text[i].match(/\.$/))
+      cache['_START'].push(text[i + 1]);
+  }
+  clear();
+};
+
+function generate() {
+  // Start with the root node
+  var currentWord = '_START';
+  var str = '';
+
+  // Generate 300 words of text
+  for (var i = 0; i < 300; i++) {
+
+    // Follow a random node, append it to the string, and move to that node
+    var rand = Math.floor(Math.random() * cache[currentWord].length);
+    str += cache[currentWord][rand];
+
+    // No more nodes to follow? Start again. (Add a period to make things look better.)
+    if (!cache[cache[currentWord][rand]]) {
+      currentWord = '_START';
+      if (!cache[currentWord][rand].match(/\.$/))
+        str += '. ';
+      else
+        str += ' ';
+    } else {
+      currentWord = cache[currentWord][rand];
+      str += ' ';
+    }
+  }
+  set(str);
+}
---
+++ program.js
@@ -0,0 +1,45 @@
+var title = "nothing"
+var gameInput = $("#game-input");
+var gameOutput  = $("#game-output");
+
+
+
+
+gameInput.keydown(function(keydownEvent) {
+  // the key code for enter is 13
+
+  if (keydownEvent.keyCode == 13) {      
+//    parseText(gameInput.val());
+    var title = gameInput.val()
+    console.log(title)
+    gameInput.val("");
+    
+    getWikiIntro(title, function(text) {
+  
+      $("#output").html(text)
+      _generate()
+      generate()
+      
+      
+    })
+    
+  
+    // Clears the source textbox
+    function clear() { document.getElementById('in').value = ''; }
+
+    // Writes to the output textbox
+    function set(v) { document.getElementById('out').value = v; }
+
+    gameInput.click(function(clickevent) {
+
+      if (clickevent) {
+        window.location.reload();
+      }
+
+    })
+    
+  }  
+})
+
+
+
